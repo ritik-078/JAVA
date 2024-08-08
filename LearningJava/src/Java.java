f58b50bc379e06ae64ee81c16a859f98c3e090ce
ritik-078 <ritikbhateley125@gmail.com>
2024-08-08 23:34:20 +0530

    Web Flux

---
+++ LearningJava/src/Java.java
@@ -0,0 +1,11 @@
+public class Java {
+    public static void main(String[] args) {
+        double d = 5.6667;
+        float f = 5.6667f;
+        char ch = 'a';
+        int a = (int)d;
+        int b = (int)f;
+        int c = (int)ch;
+        System.out.println(a +" "+ b +" "+ c);
+    }
+}
